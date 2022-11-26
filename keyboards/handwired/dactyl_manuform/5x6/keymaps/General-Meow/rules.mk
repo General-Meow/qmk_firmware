23e56de7cffcbe29f9daf262971f2ccb267f5486
General-Meow <spl.ivalis@gmail.com>
2022-11-26 11:58:37 +0000

    fixes to keymap on dactyl

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/General-Meow/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/General-Meow/rules.mk
@@ -1,8 +1,8 @@
 #EXTRAFLAGS += -flto
-
+LTO_ENABLE = yes
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
+EXTRAKEY_ENABLE = yes        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no
